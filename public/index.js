3837ee3e2973413fa198063ba3f46a7c30f3bb06
Ben <tancheanyong@gmail.com>
2022-01-14 15:34:25 +0800

    added all api key to backend

--- public/index.js
+++ public/index.js
@@ -55,7 +55,7 @@ let weather = {
         let d=new Date(this.getTimeMil(data));
 
         const months=["January","Febuary","March","April","May","June","July","August","September","October","November","December"];
-        const days=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+        const days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
         
         let day = days[d.getDay()];
         let date = d.getDate();
